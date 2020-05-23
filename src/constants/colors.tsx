302f262ec887526e88e3ae3ff580fb40a22b3cd0
Diego <jdiegomdq@gmail.com>
2020-05-23 01:16:38 -0300

    adds colors constants

--- src/constants/colors.tsx
+++ src/constants/colors.tsx
@@ -1,6 +1,11 @@
-const COLORS = {
-  ORANGE: '#FAC200', 
-  DARK_ORANGE: '#F09A0D'
-}
+export const BLACK = '#000000';
+export const DARK_GRAY = '#333333';
+export const WHITE = '#FFFFFF';
+export const LIGHT_GRAY = '#cccccc';
+export const ORANGE = '#f09a0d';
+export const LIGHT_ORANGE = '#FAC200';
+export const TURQUOISE = '#019be3';
+export const LIGHT_TURQUOISE = '#19B5FE';
 
-export default COLORS;
+
+export const PRIMARY = ORANGE;
