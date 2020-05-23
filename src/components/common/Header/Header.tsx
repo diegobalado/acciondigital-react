d63de9b528f0b2fedc89a5df9ccb622e5e412162
Diego <jdiegomdq@gmail.com>
2020-05-23 01:15:54 -0300

    refactors Nav component

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-//import { Header as GHeader, Box, Anchor, Nav } from 'grommet';
-import { Home } from 'grommet-icons';
-import Nav from './Nav';
-import config from '../../../config/NavItems'
+import { Header as GHeader, Box, Anchor, Nav } from 'grommet';
+import config from '../../../config/NavItems';
+import logo from '../../../assets/logo.png';
+import { PRIMARY } from '../../../constants/colors';
 
 type Item = {
     path: String;
@@ -11,29 +11,32 @@ type Item = {
     icon: String;
 }
 const Header = () => (
-  // <GHeader background="dark-1" pad="medium">
-  //     <Box direction="row" align="center" gap="small">
-  //       <Anchor color="white" href="/">
-  //         ShimiSun
-  //       </Anchor>
-  //     </Box>
-  //     <Nav direction="row">
-  //       {/* {config.map(item => (
-  //         <Anchor
-  //           href={item.path}
-  //           label={item.title}
-  //           key={item.title}
-  //           icon={<Home color='#F00' />}
-  //         />
-  //       ))} */}
-  //     </Nav>
-  //   </GHeader>
-  <header className="header" id="header">
-    <Link className="logo" to="/">
-      <img src="/assets/images/logo.png" alt="Accion Digital" />
-    </Link>
-    <Nav items={config} />
-  </header>
+    <GHeader background="dark-1" pad="medium" align="baseline">
+      <Box direction="row" align="center" gap="small">
+        <Anchor href="/">
+          <img src={logo} alt="Accion Digital" />
+        </Anchor>
+      </Box>
+      <Nav direction="row">
+        {config.map((item, index) => (
+          <Link to={item.path} key={index}>
+            <Anchor
+              as="span"
+              color={PRIMARY} 
+              label={item.title}
+              key={item.title}
+              icon={item.icon}
+            />
+          </Link>
+        ))}
+      </Nav>
+    </GHeader>
+  // <header className="header" id="header">
+  //   <Link className="logo" to="/">
+  //     <img src="/assets/images/logo.png" alt="Accion Digital" />
+  //   </Link>
+  //   <Nav items={config} />
+  // </header>
 );
 
 export default Header;
--- src/config/NavItems.tsx
+++ src/config/NavItems.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import { Home, Group, MailOption, CircleQuestion } from 'grommet-icons';
+import { PRIMARY } from '../constants/colors';
+
+const IconStyles = {
+  color: PRIMARY,
+}
 
 const config = [
   {
     "path": "/inicio",
     "title": "Inicio",
-    "icon": <Home />
+    "icon": <Home {...IconStyles} />
   },
   {
     "path": "/amigos",
     "title": "Amigos",
-    "icon": <Group />
+    "icon": <Group {...IconStyles} />
   },
   {
     "path": "/contacto",
     "title": "Contacto",
-    "icon": <MailOption />
+    "icon": <MailOption {...IconStyles} />
   },
   {
     "path": "/faq",
     "title": "Ayuda",
-    "icon": <CircleQuestion />
+    "icon": <CircleQuestion {...IconStyles} />
   }
 ]
 
