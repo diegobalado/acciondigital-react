a58caaac6cf6d3a553fe1268c20cad1491390d6f
Diego <jdiegomdq@gmail.com>
2020-05-23 01:32:39 -0300

    publishes docs folder

---
+++ docs/precache-manifest.724bf62e8a45ed5cb4d39b57d1cabe93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e7a4658c4f172c5a9a467ee46d38d6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa46b916ce377fa960ab",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d4a151edd5dd1ec14300",
+    "url": "/static/js/2.5bcc7912.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5bcc7912.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa46b916ce377fa960ab",
+    "url": "/static/js/main.9410e9a7.chunk.js"
+  },
+  {
+    "revision": "23d98cfab94707f3a9be",
+    "url": "/static/js/runtime-main.c6bb722c.js"
+  },
+  {
+    "revision": "e26e6631091cdb0e05ce2eaefc96c7ef",
+    "url": "/static/media/logo.e26e6631.png"
+  }
+]);
