834659f75369db49722d205de01f995c1c66131f
Diego <jdiegomdq@gmail.com>
2020-07-25 00:50:32 -0300

    changes color constants

--- src/constants/colors.tsx
+++ src/constants/colors.tsx
@@ -1,11 +1,12 @@
 export const BLACK = '#000000';
-export const DARK_GRAY = '#333333';
+export const GRAY_DARK = '#333333';
+export const GRAY_LIGHT = '#cccccc';
 export const WHITE = '#FFFFFF';
-export const LIGHT_GRAY = '#cccccc';
 export const ORANGE = '#f09a0d';
-export const LIGHT_ORANGE = '#FAC200';
+export const ORANGE_LIGHT = '#FAC200';
 export const TURQUOISE = '#019be3';
-export const LIGHT_TURQUOISE = '#19B5FE';
+export const TURQUOISE_LIGHT = '#19B5FE';
 
-
-export const PRIMARY = ORANGE;
+export const ACCENT = ORANGE_LIGHT;
+export const ACTIVE = ORANGE;
+export const PRIMARY_LIGHT = GRAY_LIGHT;
